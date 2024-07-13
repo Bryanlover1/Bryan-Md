36bb52179681f3578c0d84ac8873596f4b50224e
Bryanlover1 <165657669+Bryanlover1@users.noreply.github.com>
2024-07-13 23:04:18 +0000

    Update parole.js

--- beltah/parole.js
+++ beltah/parole.js
@@ -18,7 +18,7 @@ zokou({ nomCom: "lyrics",
 
     if (lyrics.error) return repondre("no lyrics found");
 
-    let msg = `---------Bryant-lyrics-finder--------
+    let msg = `---------Bryan-lyrics-finder--------
 
 * *Artist :* ${lyrics.artist}
 
